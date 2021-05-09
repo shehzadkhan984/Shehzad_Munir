04094e2bca0931a7badbdcb60d4ba0e20983b3cb
Shehzad <31245468+shehzadkhan984@users.noreply.github.com>
2021-05-09 22:40:23 +0500

    Initial commit

---
+++ shehzad_munir_Assign1/android/app/src/main/kotlin/com/example/shehzad_munir/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shehzad_munir
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ shehzad_munir_Assign1/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text(
+            "\n\n\nName: Shehzad Munir \nEmail: shehzadmunir986@gmail.com"),
+      ),
+    );
+  }
+}
